1f4e7937d35977355d732655b6183f2af8c5e5ac
AlexKarnei <46269001+AlexKarnei@users.noreply.github.com>
2021-01-31 23:12:19 +0300

    commit

--- 2_Algorothmization/src/com/alexkarnei/Task37.java
+++ 2_Algorothmization/src/com/alexkarnei/Task37.java
@@ -1,13 +1,11 @@
 package com.alexkarnei;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Task37 {
     public static void main() {
-        final int max = 500;
-        int[] myArray = new int[max];
-        int[] mySecondArray = new int[max];
         Random random = new Random();
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the dimension of the first array ");
@@ -15,6 +13,7 @@ public class Task37 {
             System.out.println("Insert wrong data");
         } else {
             int n = sc.nextInt();
+            int[] myArray = new int[n];
             System.out.print("First array: ");
             for (int i = 0; i < n; i++) {
                 myArray[i] = random.nextInt(100);
@@ -28,6 +27,7 @@ public class Task37 {
                 System.out.println("Insert wrong data");
             } else {
                 int m = sc.nextInt();
+                int[] mySecondArray = new int[m];
                 System.out.print("Second array: ");
                 for (int i = 0; i < m; i++) {
                     mySecondArray[i] = random.nextInt(100);
@@ -44,6 +44,11 @@ public class Task37 {
                     if (k > myArray.length - 1) {
                         System.out.println("Insert wrong data.");
                     } else {
+                        int[] resultArray = new int[myArray.length + mySecondArray.length];
+                        System.arraycopy(myArray, 0, resultArray, 0, k);
+                        System.arraycopy(mySecondArray, 0, resultArray, k, mySecondArray.length);
+                        System.arraycopy(myArray, k, resultArray, mySecondArray.length  + k, myArray.length - k);
+                        System.out.println("Result array = " + Arrays.toString(resultArray));
                     }
                 }
             }
